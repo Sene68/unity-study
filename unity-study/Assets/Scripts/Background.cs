da005267d90102fc5b37bda6752dd484def7e84c
Sene68 <senefull68@gmail.com>
2023-07-18 19:46:34 +0900

    화면 전부 내려갔을때 다시 리셋

--- unity-study/Assets/Scripts/Background.cs
+++ unity-study/Assets/Scripts/Background.cs
@@ -16,5 +16,9 @@ public class Background : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+
+        if (transform.position.y < -10) {
+            transform.position += new Vector3(0, 20f, 0);
+        }
     }
 }
