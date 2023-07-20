834a930e16c32cc333880dfa29e65a34e79e9204
Sene68 <senefull68@gmail.com>
2023-07-21 07:44:31 +0900

    플레이어 키보드 입력

---
+++ unity-study/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
+        transform.position += moveTo * moveSpeed * Time.deltaTime;
+    }
+}
