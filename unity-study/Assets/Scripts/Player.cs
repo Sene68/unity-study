e04a56fe3c965dacd29d0e3ed875fe23aa163d24
Sene68 <senefull68@gmail.com>
2023-07-22 01:55:01 +0900

    플레이어 왼쪽 오른쪽 이동제어

--- unity-study/Assets/Scripts/Player.cs
+++ unity-study/Assets/Scripts/Player.cs
@@ -16,10 +16,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        // float horizontalInput = Input.GetAxisRaw("Horizontal");
+        // float verticalInput = Input.GetAxisRaw("Vertical");
 
-        Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
-        transform.position += moveTo * moveSpeed * Time.deltaTime;
+        Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+            transform.position -= moveTo;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += moveTo;
     }
 }
