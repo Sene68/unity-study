807ba95bcd70e1bd984ce6b72cd5805617d11c3b
Sene68 <senefull68@gmail.com>
2023-07-31 08:48:58 +0900

    적이 플레이어에게 향하도록 처리

--- unity-study/Assets/Scripts/Enemy.cs
+++ unity-study/Assets/Scripts/Enemy.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    private float minY = -7;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+
+        if (transform.position.y < minY) {
+            Destroy(gameObject);
+        }
     }
 }
