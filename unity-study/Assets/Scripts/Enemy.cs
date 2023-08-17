25f06b2d4cb3debaee159c09189ff34003e0ddef
Sene68 <senefull68@gmail.com>
2023-08-17 20:17:30 +0900

    적이 죽으면 코인이 나타나도록 처리

--- unity-study/Assets/Scripts/Enemy.cs
+++ unity-study/Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
+    private GameObject coin;
+
+    [SerializeField]
     private float moveSpeed = 10f;
 
     private float minY = -7f;
@@ -41,6 +44,7 @@ public class Enemy : MonoBehaviour
             
             if (hp <= 0) {
                 Destroy(gameObject);
+                Instantiate(coin, transform.position, Quaternion.identity);
             }
             Destroy(other.gameObject);
         }
