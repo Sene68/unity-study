4d58ad1086cd93269721ab988e8bda37a6cb5662
Sene68 <senefull68@gmail.com>
2023-08-31 00:48:06 +0900

    다시 시작 버튼 동작

--- unity-study/Assets/Scripts/GameManager.cs
+++ unity-study/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -54,5 +55,10 @@ public class GameManager : MonoBehaviour
     void ShowGameOverPanel()
     {
         gameOverPanel.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
